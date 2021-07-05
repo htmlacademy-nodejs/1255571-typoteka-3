bfb48b59807fee383ef01a1cf13ea991a9a70dde
Dolgi <patemkov@gmail.com>
2021-07-05 12:30:20 +0300

    module2-task2

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,3 +5,9 @@ module.exports = {
   USER_ARGV_INDEX: 2,
   ExitCode: 1,
 };
+
+module.exports.DATA_PATH = {
+  FILE_SENTENCES_PATH: `./data/sentences.txt`,
+  FILE_TITLES_PATH: `./data/titles.txt`,
+  FILE_CATEGORIES_PATH: `./data/categories.txt`,
+};
--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -8,7 +8,8 @@ const {
   randomDate,
 } = require(`../../utils`);
 const {
-  ExitCode
+  ExitCode,
+  DATA_PATH
 } = require(`../../constants`);
 const moment = require(`moment`);
 
@@ -19,63 +20,25 @@ const CURRENT_DATE = new Date();
 let beforeDate = new Date();
 beforeDate = beforeDate.setMonth(beforeDate.getMonth() - 3);
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const readFileContent = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, `utf8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.error(chalk.red(`
+    Во время чтения файла произошла ошибка - ${err}.
+    `));
+    return [];
+  }
+};
 
-const SENTENCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
-
-const generateOffers = (count) => (
+const generateOffers = (count, sentences, titles, categories) => (
   Array(count).fill({}).map(() => ({
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    announce: shuffle(SENTENCES).slice(1, 5).join(` `),
-    fullText: shuffle(SENTENCES).slice(1, 5).join(` `),
+    title: titles[getRandomInt(0, titles.length - 1)],
+    announce: shuffle(sentences).slice(1, 5).join(` `),
+    fullText: shuffle(sentences).slice(1, 5).join(` `),
     createdDate: moment(randomDate(beforeDate, CURRENT_DATE)).format(`YYYY-MM-DD hh:mm:ss`),
-    category: [CATEGORIES[getRandomInt(0, CATEGORIES.length - 1)]]
+    category: [categories[getRandomInt(0, categories.length - 1)]]
   }))
 );
 
@@ -92,14 +55,19 @@ const writeFile = async (content) => {
 
 module.exports = {
   name: `--generate`,
-  run(args) {
+  async run(args) {
     const [count] = args;
     if (Number.parseInt(count, 10) >= MAX_COUNT) {
       console.log(chalk.red(`Не больше 1000 объявлений!`));
       process.exit(ExitCode);
     }
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
-    const content = JSON.stringify(generateOffers(countOffer));
+
+    const sentences = await readFileContent(DATA_PATH.FILE_SENTENCES_PATH);
+    const titles = await readFileContent(DATA_PATH.FILE_TITLES_PATH);
+    const categories = await readFileContent(DATA_PATH.FILE_CATEGORIES_PATH);
+
+    const content = JSON.stringify(generateOffers(countOffer, sentences, titles, categories));
     writeFile(content);
   }
 };
